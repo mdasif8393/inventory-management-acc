328263ca3ea60bc95dea936bbb6e963c0959c22b
mdasif8393 <md.asif.8393@gmail.com>
2023-01-16 20:06:05 +0600

    post data to database

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
+//SCHEMA --> MODEL --> QUERY
 //schema design
 const productSchema = mongoose.Schema({
   name: {
@@ -22,36 +23,36 @@ const productSchema = mongoose.Schema({
     required: true
   },
   price: {
-     type: Number,
-     required: true,
-     min: [0, "Price can't be negative"]
+    type: Number,
+    required: true,
+    min: [0, "Price can't be negative"]
   },
   unit: {
     type: String,
     required: true,
     enum: {
-      values: ["kg, litre", "pcs"],
+      values: ["kg", "litre", "pcs"],
       messages: ["unit value can't be {VALUE}, must be kg/litre/pcs"]
     }
   },
-  quantity:{
+  quantity: {
     type: Number,
     required: true,
     min: [0, "Quantity cant be negative"],
     validate: {
       validator: (value) => {
         const isInteger = Number.isInteger(value);
-        if(isInteger){
+        if (isInteger) {
           return true
         }
-        else{
+        else {
           return false
         }
       }
     },
     message: "Quantity must be an integer"
   },
-  status:{
+  status: {
     type: String,
     required: true,
     enum: {
@@ -59,24 +60,61 @@ const productSchema = mongoose.Schema({
       message: ["Status cant be {VALUE}"]
     }
   },
-  supplier:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Supplier"
-  },
-  categories: [{
-    name: {
-      type: String,
-      required: true
-    },
-    _id: mongoose.Schema.Types.ObjectId
-  }]
+  // supplier:{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "Supplier"
+  // },
+  // categories: [{
+  //   name: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   _id: mongoose.Schema.Types.ObjectId
+  // }]
 
-},{
+}, {
   timestamps: true
 })
+
+
+//Create Model
+const Product = mongoose.model("Product", productSchema);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
+
+//posting to database
+
+app.post("/api/v1/product", async (req, res, next) => {
+  
+  try {
+    //save
+    // const product = new Product(req.body);
+
+    // if(product.quantity == 0){
+    //   product.status = "out-of-stock"
+    // }
+
+    // const result = await product.save();
+
+    //create
+    const result = await Product.create(req.body);
+
+    res.status(200).json({
+      status: "success",
+      message: "Data inserted successfully!",
+      data: result
+    })
+  }
+  catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Data is not inserted",
+      error: error.message
+    })
+  }
+
+})
 
 module.exports = app;
